0f38d3c1fe0c1af67680725e58dc7ba765bb7b98
Sara García <gh19009@ues.edu.sv>
2022-04-27 14:44:57 -0600

    inicio de Front

---
+++ js/control/BachesDataAcces.js
@@ -0,0 +1,7 @@
+class BachesDataAccess {
+    constructor(){
+        this.BASE_URL = "http://localhost:8080/gh19009CRUD/resources/";
+
+    }
+}
+export default BachesDataAccess;
---
+++ js/control/TipoObjetoDataStore.js
@@ -0,0 +1,21 @@
+import BachesDataAccess from "./BachesDataAcces.js";
+class TipoObjetoDataStore extends BachesDataAccess{
+    constructor() {
+        super();
+    }
+
+    async contar(){
+        let promesa = fetch(this.BASE_URL+"tipoObjeto/contar",
+            {method:"GET"}
+        );
+        await promesa.then(respuesta=>respuesta.json())
+        .then(j=>console.log(j))
+        .catch(err=>console.error(err));
+        console.log("Entro a contar");
+    }
+}
+export default TipoObjetoDataStore;
+console.log("antes de contar");
+let t = new TipoObjetoDataStore();
+t.contar();
+console.log("despues de contar");
