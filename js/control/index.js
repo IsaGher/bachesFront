a0443333bf2ad8026dcaaad083f219603c65e077
Sara García <gh19009@ues.edu.sv>
2022-06-13 19:58:23 -0600

    moficacion en importaciones

--- js/control/index.js
+++
@@ -1,3 +0,0 @@
-import App from '../boundary/components/componentsEst/App.js';
-import Table from '../boundary/components/componentsEst/Table.js';
-import Form from '../boundary/components/componentsEst/Form.js';
---
+++ js/index/indexEstado.js
@@ -0,0 +1,4 @@
+import App from '../boundary/components/componentsEst/App.js';
+import Table from '../boundary/components/componentsEst/Table.js';
+import Form from '../boundary/components/componentsEst/Form.js';
+import FormCreate from '../boundary/components/componentsEst/FormCreate.js';
